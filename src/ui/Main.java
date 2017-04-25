bc4f0ca0272ead761834290a08ac64ac7575a481
oludom <mhlauffen@web.de>
2017-04-25 11:02:55 +0200

    erste Funktionalität

---
+++ src/ui/Main.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Micha Heiß
+ */
+public class Main extends Application{
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        System.out.println("Jetzt weiß ich auch, warum Sie Köpfe haben, soll ich's sagen?");
+        System.out.println("Sie müssen dann das viele Stro nicht in den Händen tragen.");
+        System.exit(0);
+
+    }
+}
